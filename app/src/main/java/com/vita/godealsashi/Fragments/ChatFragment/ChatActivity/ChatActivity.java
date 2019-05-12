d4f3290361af4ab176b4725dfb070018bcd9ceb7
vitodon <45338513+vitonman@users.noreply.github.com>
2019-05-12 20:24:22 +0300

    something wrong with gradle

---
+++ app/src/main/java/com/vita/godealsashi/Fragments/ChatFragment/ChatActivity/ChatActivity.java
@@ -0,0 +1,15 @@
+package com.vita.godealsashi.Fragments.ChatFragment.ChatActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.vita.godealsashi.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
